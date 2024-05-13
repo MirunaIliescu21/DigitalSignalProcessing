fc7ca1fad7f39911b499127759c3fb8d3d70a4e1
Miruna Iliescu <miruna.iliescu@gmail.com>
2024-05-13 21:29:09 +0300

    task1 yupi

--- src/numerical_music/low_pass.m
+++ src/numerical_music/low_pass.m
@@ -1,4 +1,20 @@
 function signal = low_pass(signal, fs, cutoff_freq)
-  signal = 0;
+  % Transformarea Fourier a semnalului signal
+  fourier = fft(signal);
+  
+  % Calculez vectorul de frecventa
+  total = length(signal);
+  f = (0 : total - 1) * fs / total;
+  
+  % Creez o masca pt filtru
+  mask = f <= cutoff_freq;
+  
+  % Calculez produsul Hadamard intre transf Fouriei si masca
+  fourier_filtered = fourier .* mask';
+  
+  % Calculez inversa transformatei Fourier
+  filtered_signal = ifft(fourier_filtered);
+  
+  % Normalizez semnalul
+  signal = filtered_signal / max(abs(filtered_signal));
 endfunction
-
